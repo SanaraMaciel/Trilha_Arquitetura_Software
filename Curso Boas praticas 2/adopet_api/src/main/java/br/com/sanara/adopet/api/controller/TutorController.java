317b00111376cbea0e71f24eb6f188e16faae6b1
sanara <sanara@cpqd.com.br>
2025-02-25 10:18:28 -0300

    adicionado certificados do curso

--- Curso Boas praticas 2/adopet_api/src/main/java/br/com/sanara/adopet/api/controller/TutorController.java
+++ Curso Boas praticas 2/adopet_api/src/main/java/br/com/sanara/adopet/api/controller/TutorController.java
@@ -2,8 +2,6 @@ package br.com.sanara.adopet.api.controller;
 
 import br.com.sanara.adopet.api.dto.TutorDto;
 import br.com.sanara.adopet.api.exception.ValidacaoException;
-import br.com.sanara.adopet.api.model.Tutor;
-import br.com.sanara.adopet.api.repository.TutorRepository;
 import br.com.sanara.adopet.api.service.TutorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
--- Curso Boas praticas 2/adopet_api/src/main/java/br/com/sanara/adopet/api/service/TutorService.java
+++ Curso Boas praticas 2/adopet_api/src/main/java/br/com/sanara/adopet/api/service/TutorService.java
@@ -5,7 +5,6 @@ import br.com.sanara.adopet.api.exception.ValidacaoException;
 import br.com.sanara.adopet.api.model.Tutor;
 import br.com.sanara.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
